95dbcf7da39c398ebb35c38a5360d150140d5026
Zim Kalinowski <zim.kalinowski@zoho.com>
2021-11-25 16:47:51 +0800

    adding files

---
+++ index.js
@@ -0,0 +1,6 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+const { triggers } = require('botbuilder-dialogs-adaptive-runtime-integration-azure-functions');
+
+module.exports = triggers(process.cwd(), "settings");
